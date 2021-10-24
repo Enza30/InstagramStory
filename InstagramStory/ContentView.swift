70b06364787ca30eca2d21d34ad3e646fb2eed34
Farendza <farendza.muda@gmail.com>
2021-10-25 06:26:02 +0700

    Initial Commit

---
+++ InstagramStory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramStory
+//
+//  Created by Farendza Muda on 25/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramStory/InstagramStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoryApp.swift
+//  InstagramStory
+//
+//  Created by Farendza Muda on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
