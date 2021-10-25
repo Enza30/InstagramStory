e142559d02fc12a12af2baab562e30edab13b396
Farendza <farendza.muda@gmail.com>
2021-10-25 07:38:51 +0700

    Input Gambar and some view

--- InstagramStory/ContentView.swift
+++ InstagramStory/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Pecobaan pertama")
-            .padding()
+        LoadingRectangle(progress: <#CGFloat#>)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct LoadingRectangle: View {
+    var progress : CGFloat
+    
+    var body: some View{
+        GeometryReader { geometry in
+            ZStack(alignment: .leading){
+                Rectangle()
+                    .foregroundColor(Color.white.opacity(0.3))
+                    .cornerRadius(5)
+                Rectangle()
+                    .frame(width: geometry.size.width * self.progress , height: nil, alignment: .leading)
+                    .foregroundColor(Color.white.opacity(0.9))
+                    .cornerRadius(5)
+            }
+        }
     }
 }
